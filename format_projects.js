894efc5287156e0e8998386a2435119680cb7f03
Alya <alya.saleh@hotmail.com>
2017-10-30 00:56:59 +0300

    Add files via upload

---
+++ format_projects.js
@@ -0,0 +1,140 @@
+projects = [
+  {
+    title: "Inventory App",
+    description: "<li id=\"li_projects\">Project <b>Inventory App</b> of Android Basics Nanodegree at <a href=\"https://www.udacity.com/course/android-basics-nanodegree-by-google--nd803\" \
+    target=\"_blank\">Udacity.com</a></li>\
+	<li id=\"li_projects\">Final project of the Udacity course Android Development for Beginners</li>\
+	<li id=\"li_projects\">Images sources (<a href=\"https://www.amazon.com\"\
+    target=\"_blank\">1</a>,<a href=\"https://resources.cin7.com/wp-content/uploads/2015/11/what-is-inventory.png\" \
+    target=\"_blank\">2</a>)</li>\
+    <br><br><b>Description</b> \
+    <li id=\"li_projects\">Keep track of your inventory</li> \
+    <li id=\"li_projects\">Sell items with one click</li> \
+    <li id=\"li_projects\">Edit items</li> \
+    <li id=\"li_projects\">Show pictures from the items</li> \
+    <li id=\"li_projects\">Order more items with e-mail</li> \
+    <li id=\"li_projects\">Delete one or all of your items</li>",
+    github: "https://github.com/alyas92/InventoryApp",
+    screenshot: "src/inventory.png"
+  },
+    {
+    title: "Habit Tracker",
+    description: "<li id=\"li_projects\">Project <b>Habit Tracker App</b> of Android Developer Nanodegree at <a href=\"https://www.udacity.com/course/android-basics-nanodegree-by-google--nd803\" \
+    target=\"_blank\">Udacity.com</a></li>\
+    <br><br><b>Description</b> \
+    <li id=\"li_projects\">Help users track and maintain healthy habits</li> \
+    <li id=\"li_projects\">Using a SQLite</li>",
+    github: "https://github.com/alyas92/HabitTrackerApp",
+    screenshot: "src/habit_tracker.png"
+  },
+  {
+    title: "News App",
+    description: "<li id=\"li_projects\">Project <b>News App</b> of Android Basics Nanodegree at <a href=\"https://www.udacity.com/course/android-basics-nanodegree-by-google--nd803\" \
+    target=\"_blank\">Udacity.com</a></li>\
+    <br><br><b>Description</b> \
+    <li id=\"li_projects\">Get the latest Android News</li> \
+    <li id=\"li_projects\">Retrieves data from The Guardian API</li> \
+    <li id=\"li_projects\">Only the first 15 results will be displayed</li> \
+    <li id=\"li_projects\">Swipe down to refresh the news list</li> \
+    <li id=\"li_projects\">Tap any item to open the article in your browser</li>",
+    github: "https://github.com/alyas92/NewsApp",
+    screenshot: "src/news.png"
+  },
+  {
+    title: "Book Listing",
+    description: "<li id=\"li_projects\">Project <b>Book Listing</b> of Android Basics Nanodegree at <a href=\"https://www.udacity.com/course/android-basics-nanodegree-by-google--nd803\" \
+    target=\"_blank\">Udacity.com</a></li>\
+    <br><br><b>Description</b> \
+    <li id=\"li_projects\">Search books by author or title.</li> \
+    <li id=\"li_projects\">Retrieves data from Google Books API.</li> \
+    <li id=\"li_projects\">Only the first 10 results will be displayed.</li> \ ",
+    github: "https://github.com/alyas92/BookListing",
+    screenshot: "src/book_listing.png"
+  },
+  {
+    title: "Riyadh Guide",
+    description: "<li id=\"li_projects\">Project <b>Tour Guide</b> of Android Basics Nanodegree at <a href=\"https://www.udacity.com/course/android-basics-nanodegree-by-google--nd803\" \
+    target=\"_blank\">Udacity.com</a></li>\
+	<li id=\"li_projects\"><a href=\"https://www.tripadvisor.com/Attractions-g293995-Activities-Riyadh_Riyadh_Province.html\" \
+    target=\"_blank\">Images source</a></li>\
+    <br><br><b>Description</b> \
+    <li id=\"li_projects\">Tour Guide app of Riyadh.</li> \
+    <li id=\"li_projects\">It contains a list of Hotels, resturants, malls and museums. Also some useful information for travellers.</li> \ ",
+    github: "https://github.com/alyas92/Riyadhtourguide",
+    screenshot: "src/device-2017-10-18-162812.PNG"
+  },
+  {
+    title: "Music Player",
+    description: "<li id=\"li_projects\">Project <b>Musical Structure</b> of Android Basics Nanodegree at <a href=\"https://www.udacity.com/course/android-basics-nanodegree-by-google--nd803\" \
+    target=\"_blank\">Udacity.com</a></li>\
+    <li id=\"li_projects\">Images sources (<a href=\"https://static.annahar.com/storage/attachments/227/2vATfvSc_807818_649866_438325_highres.jpg\"\
+    target=\"_blank\">1</a>,<a href=\"https://thumb7.shutterstock.com/display_pic_with_logo/1864778/192874409/stock-vector-vector-pink-music-icon-eps-192874409.jpg\" \
+    target=\"_blank\">2</a>)</li>\
+    <br><br><b>Description</b> \
+    <li id=\"li_projects\">Structure of a music player app</li>\ ",
+    github: "https://github.com/alyas92/MusicPlayerApp",
+    screenshot: "src/music.png"
+  },
+  {
+    title: "Country Quiz",
+    description: "<li id=\"li_projects\">Project <b>Quiz App</b> of Android Basics Nanodegree at <a href=\"https://www.udacity.com/course/android-basics-nanodegree-by-google--nd803\" \
+    target=\"_blank\">Udacity.com</a></li>\
+	<li id=\"li_projects\"><a href=\"https://openclipart.org/image/800px/svg_to_png/271128/kawaii_earth.png\" \
+    target=\"_blank\">Image source</a></li>\
+    <br><br><b>Description</b> \
+    <li id=\"li_projects\">5 Question's quizz about Countries</li> \
+    <li id=\"li_projects\">It had to contain radio buttons, checkboxes and EditText</li> \
+    <li id=\"li_projects\">Final score is shown with a Toast</li>",
+    github: "https://github.com/alyas92/CountryQuiz",
+    screenshot: "src/quiz.png"
+  },
+  
+  {
+    title: "Football Score Keeper",
+    description: "<li id=\"li_projects\">Project <b>Score Keeper App</b> of Android Basics Nanodegree at <a href=\"https://www.udacity.com/course/android-basics-nanodegree-by-google--nd803\" \
+    target=\"_blank\">Udacity.com</a></li>",
+    github: "https://github.com/alyas92/FootballScoreKeeper",
+    screenshot: "src/score_keeper.png"
+  },
+  {
+    title: "King Restaurant",
+    description: "<li id=\"li_projects\">Project <b>Build a Single Screen App</b> of Android Basics Nanodegree at <a href=\"https://www.udacity.com/course/android-basics-nanodegree-by-google--nd803\" \
+    target=\"_blank\">Udacity.com</a></li>",
+    github: "https://github.com/alyas92/KingRestaurant",
+    screenshot: "src/king.png"
+  },
+];
+
+android = 0
+
+$(document).ready(function() {
+
+  projects.forEach(function(project) {
+      createCard(project);
+  });
+
+  function createCard(project){
+    if (android % 2 == 0) {
+      $("#android").append("<div class=\"row\"></div>");
+    }
+    android++;
+    $("#android" ).children().last().append(
+        "<div class=\"col s12 m6\">" +
+        "  <div class=\"card horizontal\">" +
+        "    <div class=\"card-image card-image-padding\">" +
+        "      <img src=\"" + project.screenshot + "\" alt=\"Image\"/>" +
+        "    </div>" +
+        "    <div class=\"card-stacked\">" +
+        "    <div class=\"card-content\">" +
+        "      <span class=\"card-title purple-text text-darken-4\">" + project.title + "</span>" +
+        "      <p>" + project.description + "</p>" +
+        "    </div>" +
+        "    <div class=\"card-action\">" +
+        "      <a href=\"" +project.github +"\" target=\"_blank\">GitHub</a>" +
+        "    </div>" +
+        "  </div>" +
+	    "  </div>" +
+        "</div>");
+  };
+});
+
